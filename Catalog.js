8bc282314290c3a10767e246fd2a40cc029f577f
Terry Gu <gyoushijie@gmail.com>
2022-05-07 01:15:57 -0700

    :memo: Added Catalog

---
+++ Catalog.js
@@ -0,0 +1,10 @@
+export const Catalog = {
+    Uncategorized: [
+        {
+            title: "Hello World",
+            description: "The first post of the site, for testing purposes",
+            author: "RiverNotHere",
+            fileName: "HelloWorld"
+        }
+    ]
+}
